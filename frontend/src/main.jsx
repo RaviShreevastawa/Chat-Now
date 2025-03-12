c523c53d2560ad778a8eb5fa5424e95e2a7d3fe4
Ravi <ravishreevastawa@gmail.com>
2025-03-12 10:47:54 +0530

    Last Commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthContextProvider } from './Context/AuthContext.jsx'
-import { SocketContextProvider } from './context/SocketContext.jsx'
+import { SocketContextProvider } from './Context/SocketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
