3c154b883fa074fc4a32faddd0872ea95719c1bd
Ravi <ravishreevastawa@gmail.com>
2025-03-12 09:49:41 +0530

    last commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path'
 import cookieParser from 'cookie-parser';
 
 import authRoute from "./src/routes/auth.routes.js"
@@ -11,6 +12,8 @@ import {app, server} from "./src/socket/socket.js"
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 app.use("/api/users", userRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectDB();
